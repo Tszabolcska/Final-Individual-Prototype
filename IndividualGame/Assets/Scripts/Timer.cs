d7eb22b6b0af345139eea5e0182eee9b80fce67f
Tszabolcska <35670524+Tszabolcska@users.noreply.github.com>
2018-12-05 20:06:34 -0700

    My game

---
+++ IndividualGame/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Diagnostics;
+
+public class Timer : MonoBehaviour {
+
+    public Text timerText;
+    public float t;
+
+    // Use this for initialization
+    void Start () {
+         
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        t = Time.timeSinceLevelLoad;
+        string seconds = (t).ToString("f0");
+        timerText.text = seconds;
+
+        if (t >= 10)
+        {
+            Health.health -= 0.2f;
+        }
+        else if (t >= 50)
+        {
+            Health.health -= 0.3f;
+        }
+    }
+}
